181198a79dd9607f8d4931b9e52643f653ef92ac
Padraic Edwards <padraic.edwards@ie.ibm.com>
2020-10-27 15:41:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to using . \'s  use the Delivery Service');
+  res.send('Welcome to using . \'s  use the  Service');
 });
 
 app.listen(PORT);
